e88d1c2ac23aa1f3f3603b324b368d9ae7dbb044
Ehinomen <ehinomen.asikagbon@pau.edu.ng>
2025-10-08 18:17:21 +0100

    My second commit

---
+++ week-2/practice_2.rs
@@ -0,0 +1,6 @@
+fn main() {
+    println!(); // prints just a
+    println!("hello");//prints
+    println!("format {} arguments", "some");
+    println!("My name is {}. I am the class rep of stream 1", "Dagogo William-Jumbo");
+}
