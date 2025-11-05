16b20536eacded37bc97d7c6c9043de1479d83c6
Ehinomen <ehinomen.asikagbon@pau.edu.ng>
2025-11-05 17:05:59 +0100

    using format

---
+++ week-6/practice_7/target/debug/practice_7.d
@@ -0,0 +1,1 @@
+C:\Users\personal\OneDrive\Dokumente\e.asikagbonCOS101\week-6\practice_7\target\debug\practice_7.exe: C:\Users\personal\OneDrive\Dokumente\e.asikagbonCOS101\week-6\practice_7\src\main.rs
