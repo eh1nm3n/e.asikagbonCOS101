379c999b1a0417d653a181668367e8edfb051bb3
Ehinomen <ehinomen.asikagbon@pau.edu.ng>
2025-11-05 16:48:11 +0100

    push string

---
+++ week-6/practice_4/target/debug/deps/practice_4.d
@@ -0,0 +1,5 @@
+C:\Users\personal\OneDrive\Dokumente\e.asikagbonCOS101\week-6\practice_4\target\debug\deps\practice_4.d: src\main.rs
+
+C:\Users\personal\OneDrive\Dokumente\e.asikagbonCOS101\week-6\practice_4\target\debug\deps\practice_4.exe: src\main.rs
+
+src\main.rs:
