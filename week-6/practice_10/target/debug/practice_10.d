274c3fd5ff29b330be4cce1e3a2200897c7c79a4
Ehinomen <ehinomen.asikagbon@pau.edu.ng>
2025-11-05 17:22:57 +0100

    logical operators

---
+++ week-6/practice_10/target/debug/practice_10.d
@@ -0,0 +1,1 @@
+C:\Users\personal\OneDrive\Dokumente\e.asikagbonCOS101\week-6\practice_10\target\debug\practice_10.exe: C:\Users\personal\OneDrive\Dokumente\e.asikagbonCOS101\week-6\practice_10\src\main.rs
