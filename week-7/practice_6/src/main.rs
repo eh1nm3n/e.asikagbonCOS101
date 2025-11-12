85e4e400c3601a7d2a9a331287d84d579361b4b1
Ehinomen <ehinomen.asikagbon@pau.edu.ng>
2025-11-12 17:15:31 +0100

    pass by reference

--- week-7/practice_6/src/main.rs
+++ week-7/practice_6/src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
     let mut num:i32 = 5;
-    mutate_num_t0_zero(&mut num);
+    mutate_num_to_zero(&mut num);
     println!("The value of num is:{}",num);
 }
-fn mutate_num_to_zero(paramum_num:&mut i32){
+fn mutate_num_to_zero(param_num:&mut i32){
     *param_num = *param_num*0;
     println!("param_num value is: {}",param_num);
 }
