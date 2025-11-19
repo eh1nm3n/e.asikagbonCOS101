f14cb344fffb742001b9f62704be9d182e5fdd71
Ehinomen <ehinomen.asikagbon@pau.edu.ng>
2025-11-19 18:22:02 +0100

    mutable slice in rust

--- week 8/practice_12/src/main.rs
+++ week 8/practice_12/src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+fn main(){
+    let mut colors = ["red","green","yellow","white"];
+    println!("\nOriginal array = {:?}",colors);
+    let sliced_colors = &mut colors[1..3];
+    println!("First slice = {:?}",sliced_colors);
+    sliced_colors[1] = "purple";
+    println!("Changed slice = {:?}",sliced_colors);
 }
