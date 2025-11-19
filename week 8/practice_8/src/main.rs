8cd03b7a2ce81924897a4335dcf8312be4b73720
Ehinomen <ehinomen.asikagbon@pau.edu.ng>
2025-11-19 17:27:02 +0100

    mutable tuple

--- week 8/practice_8/src/main.rs
+++ week 8/practice_8/src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
-    println!("Hello, world!");
+fn main(){
+    let mut mountain_heights = ("Everest", 8848,"Fishtail",6993);
+    println!("Original tuple = {:?}",mountain_heights);
+    mountain_heights.2 = "Lhotse";
+    mountain_heights.3 = 8516;
+    println!("Changed tuple = {:?}",mountain_heights);
 }
