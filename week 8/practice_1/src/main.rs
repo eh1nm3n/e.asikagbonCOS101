d6d8f56c26b2dadd41f51497b599cd95c80b4c0d
Ehinomen <ehinomen.asikagbon@pau.edu.ng>
2025-11-19 16:19:12 +0100

    Creating vectors

---
+++ week 8/practice_1/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let e:Vec<i64> = Vec::new();
+    println!("\nThe length of Vec::new is: {}",e.len());
+    let e = vec!["Ehinomen","Jane","Chris","Lisa","Sam"];
+    println!("\nThe length of vec macro is: {}",e.len());
+}
