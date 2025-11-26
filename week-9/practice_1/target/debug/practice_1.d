3d67de87c4cedc629b4e18361d8c499d479fd870
eh1nm3n <ehinomen.asikagbon@pau.edu.ng>
2025-11-26 16:23:47 +0100

    had a mistake

--- week-9/practice_1/target/debug/practice_1.d
+++
@@ -1,1 +0,0 @@
-C:\Users\personal\OneDrive\Dokumente\e.asikagbonCOS101\week-9\practice_1\target\debug\practice_1.exe: C:\Users\personal\OneDrive\Dokumente\e.asikagbonCOS101\week-9\practice_1\src\main.rs
--- week-9/practice_3/src/main.rs
+++
@@ -1,5 +0,0 @@
-use std::fs;
-fn main(){
-    fs::remove_file("../practice_1/data.txt").expect("could not remove file");
-    println!("file is removed");
-}
